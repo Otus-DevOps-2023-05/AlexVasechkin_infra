f05af0b97e166359927a4f1b3977a94fe867d030
Александр <administrator@MacBook-Pro-Aleksandr.local>
2023-08-13 12:45:09 +0300

    провизионеры

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,4 +39,22 @@ resource "yandex_compute_instance" "app" {
     subnet_id = "e9bmco4kdh3g7iefnv0h"
     nat       = true
   }
+
+  connection {
+    type = "ssh"
+    host = yandex_compute_instance.app.network_interface.0.nat_ip_address
+    user = "ubuntu"
+    agent = false
+    # путь до приватного ключа
+    private_key = file("~/.ssh/yc")
+  }
+
+  provisioner "file" {
+    source = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
+
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  }
 }
