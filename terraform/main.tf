121d6fa2073deb53c0b142523e72312d620e8f43
Александр <administrator@MacBook-Pro-Aleksandr.local>
2023-08-13 13:13:24 +0300

    самостоятельные задания

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,9 +9,9 @@ terraform {
 
 provider "yandex" {
   service_account_key_file = var.service_account_key_file
-  cloud_id = var.cloud_id
-  folder_id = var.folder_id
-  zone = var.zone
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.zone
 }
 
 resource "yandex_compute_instance" "app" {
@@ -20,9 +20,9 @@ resource "yandex_compute_instance" "app" {
   platform_id = "standard-v3"
 
   resources {
-    cores  = 2
+    cores         = 2
     core_fraction = 20
-    memory = 2
+    memory        = 2
   }
 
   boot_disk {
@@ -43,16 +43,16 @@ resource "yandex_compute_instance" "app" {
   }
 
   connection {
-    type = "ssh"
-    host = yandex_compute_instance.app.network_interface.0.nat_ip_address
-    user = "ubuntu"
+    type  = "ssh"
+    host  = yandex_compute_instance.app.network_interface.0.nat_ip_address
+    user  = "ubuntu"
     agent = false
     # путь до приватного ключа
-    private_key = file("~/.ssh/yc")
+    private_key = file(var.private_key_path)
   }
 
   provisioner "file" {
-    source = "files/puma.service"
+    source      = "files/puma.service"
     destination = "/tmp/puma.service"
   }
 
