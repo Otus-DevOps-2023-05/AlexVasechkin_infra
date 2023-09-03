07121b2f7b6a9575a703db277093bead9a1755c7
Александр <administrator@MacBook-Pro-Aleksandr.local>
2023-09-03 18:31:48 +0300

    конфиги разнесены по файлам

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ terraform {
       source = "yandex-cloud/yandex"
     }
   }
-  # required_version = ">= 0.13"
+
   required_version = ">= 0.12.19"
 }
 
@@ -13,64 +13,4 @@ provider "yandex" {
   cloud_id                 = var.cloud_id
   folder_id                = var.folder_id
   zone                     = var.zone
-}
-
-resource "yandex_compute_instance" "app" {
-  name = "reddit-app"
-
-  platform_id = "standard-v3"
-
-  resources {
-    cores         = 2
-    core_fraction = 20
-    memory        = 2
-  }
-
-  boot_disk {
-    initialize_params {
-      # Указать id образа созданного в предыдущем домашем задании
-      image_id = var.image_id
-    }
-  }
-
-  network_interface {
-    # Указан id подсети default-ru-central1-a
-    subnet_id = yandex_vpc_subnet.app-subnet.id
-    nat       = true
-  }
-
-  metadata = {
-    ssh-keys = "ubuntu:${file(var.public_key_path)}"
-  }
-
-  connection {
-    type  = "ssh"
-    host  = yandex_compute_instance.app.network_interface.0.nat_ip_address
-    user  = "ubuntu"
-    agent = false
-    # путь до приватного ключа
-    private_key = file(var.private_key_path)
-  }
-
-  provisioner "file" {
-    source      = "files/puma.service"
-    destination = "/tmp/puma.service"
-  }
-
-  provisioner "remote-exec" {
-    script = "files/deploy.sh"
-  }
-
-  depends_on = [yandex_vpc_network.app-network]
-}
-
-resource "yandex_vpc_network" "app-network" {
-  name = "reddit-app-network"
-}
-
-resource "yandex_vpc_subnet" "app-subnet" {
-  name           = "reddit-app-subnet"
-  zone           = "ru-central1-a"
-  network_id     = "${yandex_vpc_network.app-network.id}"
-  v4_cidr_blocks = ["192.168.10.0/24"]
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -15,12 +15,17 @@ variable "public_key_path" {
 variable "private_key_path" {
   description = "Path to the private key used for ssh access"
 }
-variable "image_id" {
-  description = "Disk image"
-}
 variable "subnet_id" {
   description = "Subnet"
 }
 variable "service_account_key_file" {
   description = "Service account key file"
+}
+variable "app_disk_image" {
+  description = "Disk image for reddit app"
+  default = "reddit-app-base"
+}
+variable "db_disk_image" {
+  description = "Disk image for reddit db"
+  default = "reddit-db-base"
 }
