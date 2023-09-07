bbc38d7f619565ad240cf3b6445d30cba5695bc9
Александр <administrator@MacBook-Pro-Aleksandr.local>
2023-09-07 09:22:28 +0300

    убрал версию провайдера

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -2,7 +2,6 @@ terraform {
   required_providers {
     yandex = {
       source = "yandex-cloud/yandex"
-      version = "0.97.0"
     }
   }
 
@@ -17,7 +16,7 @@ provider "yandex" {
 }
 
 module "app" {
-  source           = "./modules/app"
+  source           = "../modules/app"
   public_key_path  = var.public_key_path
   private_key_path = var.private_key_path
   app_disk_image   = var.app_disk_image
@@ -25,7 +24,7 @@ module "app" {
 }
 
 module "db" {
-  source          = "./modules/db"
+  source          = "../modules/db"
   public_key_path = var.public_key_path
   db_disk_image   = var.db_disk_image
   subnet_id       = var.subnet_id
