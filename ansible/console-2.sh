1ed50393e3b320c078bd2c9ee9df48e3e34bced2
Александр <administrator@MacBook-Pro-Aleksandr-2.local>
2023-10-22 15:42:33 +0300

    handler перезапуска сервиса mongodb

--- ansible/console-2.sh
+++ ansible/console-2.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 ansible-playbook reddit_app.yml --check --limit db
 # --limit - ограничиваем группу хостов, для которых применить плейбук
+
+# применим playbook
+ansible-playbook reddit_app.yml --limit db
