3603cd2ca4d21cbefca4b85a370df4dc2888c1bf
Александр <administrator@MacBook-Pro-Aleksandr-2.local>
2023-10-28 19:23:14 +0300

    создание виртуальной машины с помощью molecule

--- ansible/console-4.sh
+++ ansible/console-4.sh
@@ -4,3 +4,9 @@ vagrant provision dbserver
 vagrant provision appserver
 
 molecule init scenario --scenario-name default -r db -d vagrant
+
+molecule create
+
+molecule list
+
+molecule login -h instance
